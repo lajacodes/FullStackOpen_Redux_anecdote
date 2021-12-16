9c0aa7ba9a9a7c0b24f24c01e1b3e72af0ee2401
Goodness Adelaja <lajaololade@gmail.com>
2021-12-16 18:29:26 +0100

    6.3: anecdotes, step1

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,29 +1,46 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
-  }
-}
+    votes: 0,
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case "VOTE_ACTION": {
+      const id = action.data;
+      const voteToCount = state.find((n) => n.id === id);
+      console.log(voteToCount);
+      const changedVote = {
+        ...voteToCount,
+        votes: voteToCount.votes + 1,
+      };
+      return state.map((votes) => (votes.id !== id ? votes : changedVote));
+    }
+    default:
+      return state;
+  }
+};
 
-  return state
-}
+export const voteAction = (data) => {
+  return {
+    type: "VOTE_ACTION",
+    data,
+  };
+};
 
-export default reducer
+export default reducer;
